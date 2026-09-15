6e499f65e918717424163b3e64615b8360dd9b59
Raeda <saudiraeda83@gmail.com>
2026-09-15 12:23:14 +0300

    develop bransh

--- src/test/java/test1/seleniumproject/Githup.java
+++ src/test/java/test1/seleniumproject/Githup.java
@@ -1,5 +1,16 @@
 package test1.seleniumproject;
 
+import org.junit.Test;
+
 public class Githup {
+	
+	
+	@Test
+	public void first() {
+		
+		
+		
+		System.out.println("develop bransh");
+	}
 
 }
