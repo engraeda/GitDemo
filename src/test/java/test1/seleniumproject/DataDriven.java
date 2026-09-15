6ee226e7081e3fb3895adc5813ef31668b4b0c7d
Raeda <saudiraeda83@gmail.com>
2026-09-15 11:26:36 +0300

    this from qa team member

--- src/test/java/test1/seleniumproject/DataDriven.java
+++ src/test/java/test1/seleniumproject/DataDriven.java
@@ -1,10 +1,26 @@
 package test1.seleniumproject;
 
+import org.junit.Test;
+
 public class DataDriven {
+@Test
+public void print() {
+	
+	
+	System.out.println("how r u?");
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+@Test
+public void print2() {
+	
+	
+	System.out.println("how r u.....?");
+}
 
-	}
-
+@Test
+public void print3() {
+	
+	
+	System.out.println("how r u.....?");
+}
 }
---
+++ src/test/java/test1/seleniumproject/Githup.java
@@ -0,0 +1,5 @@
+package test1.seleniumproject;
+
+public class Githup {
+
+}
